cf3adadae1304b10fe30068d29a06cf1513e0548
riansabrido-cs <rian.sabrido@neu.edu.ph>
2026-09-04 20:22:07 +0800

    Create Light.java

---
+++ commandPattern/Light.java
@@ -0,0 +1,10 @@
+package commandPattern;
+
+public class Light {
+  public String turnOn() {
+    return "Light is turned on"
+  }
+  public String turnOff() {
+    return "Light is turned off"
+  }
+}
